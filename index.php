24e9cc00a19d34bc2bb8237380e1fd8bd145ba2d
Md: Rakibul Hasan <49594744+hasan972@users.noreply.github.com>
2020-11-17 00:30:57 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post" action="index.php">
+Username:<br/>
+<input type="text" name="Username"<br/>
+password<br/>
+<input type="password"name="password"<br/>
+<input type="submit" value="Login">
+</from>
+
+</body>
+</html>
